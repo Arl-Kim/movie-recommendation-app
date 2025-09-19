4119787b91ad9881f7b9a1068d2be9b52da3be6f
Allan Kimutai Tum <allankimutaitum@gmail.com>
2025-09-19 23:56:34 +0300

    Implement auto scroll to top when route changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Home from "./pages/Home/Home.tsx";
 import Recommendations from "./pages/Recommendations/Recommendations.tsx";
 import News from "./pages/News/News.tsx";
 import Footer from "./components/Footer/Footer.tsx";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop.tsx";
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route index element={<Home />} />
